411c835575bea014fa7e1c3fca952307a26961d7
itsukiisamu <33346277+itsukiisamu@users.noreply.github.com>
2026-05-08 12:16:49 +0900

    Update vite.config.ts take1
    vite.config.ts を少し修正する
    GitHub Pagesで公開するためには、「このアプリは /make_a_curry/ というURLで動くよ」と教えてあげる必要があります。
    リポジトリにある vite.config.ts を開きます（一番上の階層にあるはずです）。
    ペンマーク（編集）ボタンを押します。
    中身を以下のように書き換えます（base という一行を追加するだけです）。

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/make_a_curry/', // これを追加！リポジトリ名と同じにする必要があります
 })
